0fec28e5ca2a16b0ee7bb7b28afb38fddc3ea29c
Junil Ko <junil@mocapot.com>
2026-03-06 16:40:06 +0900

    프로젝트 시작

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(PlayerInput))]
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform headTransform;
+    
+    private Animator _animator;
+    private PlayerInput _playerInput;
+    private CharacterController _characterController;
+    
+    // 애니메이션 키
+    public static readonly int PlayerAniParamMove = Animator.StringToHash("move");
+    public static readonly int PlayerAniParamMoveSpeed = Animator.StringToHash("move_speed");
+
+    public enum EPlayerState
+    {
+        None, Idle, Move
+    }
+    
+    // 현재 상태에 대한 정보
+    public EPlayerState PlayerState { get; private set; }
+    
+    // 상태와 상태 객체를 담고있는 Dictionary
+    private Dictionary<EPlayerState, IPlayerState> _playerStates;
+    
+    private void Awake()
+    {
+        // 컴포넌트 초기화
+        _animator = GetComponent<Animator>();
+        _playerInput = GetComponent<PlayerInput>();
+        _characterController = GetComponent<CharacterController>();
+        
+        // 상태 객체 초기화
+        var idlePlayerState = new IdlePlayerState(this, _animator, _playerInput);
+        var movePlayerState = new MovePlayerState(this, _animator, _playerInput);
+
+        _playerStates = new Dictionary<EPlayerState, IPlayerState>
+        {
+            { EPlayerState.Idle, idlePlayerState },
+            { EPlayerState.Move, movePlayerState },
+        };
+        
+        // 카메라 할당
+        var playerCamera = Camera.main;
+        if (playerCamera != null)
+        {
+            _playerInput.camera = playerCamera;
+            playerCamera.GetComponent<CameraController>().SetTarget(headTransform, _playerInput);
+        }
+    }
+
+    private void Start()
+    {
+        PlayerState = EPlayerState.None;
+    }
+
+    private void Update()
+    {
+        if (PlayerState != EPlayerState.None) _playerStates[PlayerState].Update();
+    }
+
+    // 새로운 상태를 할당하는 함수
+    public void SetState(EPlayerState state)
+    {
+        if (PlayerState == state) return;
+        if (PlayerState != EPlayerState.None) _playerStates[PlayerState].Exit();
+        PlayerState = state;
+        if (PlayerState != EPlayerState.None) _playerStates[PlayerState].Enter();
+    }
+}
---
+++ Assets/Scripts/Player/State/IdlePlayerState.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class IdlePlayerState : PlayerState, IPlayerState
+{
+    public IdlePlayerState(PlayerController playerController, Animator animator, PlayerInput playerInput)
+     : base(playerController, animator, playerInput) { }
+
+    public void Enter()
+    {
+        
+    }
+
+    public void Update()
+    {
+        if (_playerInput.actions["Move"].IsPressed())
+        {
+            _playerController.SetState(PlayerController.EPlayerState.Move);
+        }
+    }
+
+    public void Exit()
+    {
+        
+    }
+}
