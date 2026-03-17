85a2c83b859a536d712995c72673f3f36c2a89e9
goyadb <junil@mocapot.com>
2026-03-17 10:49:54 +0900

    로딩화면 구현

---
+++ Assets/Scripts/Prop/DoorController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    [SerializeField] private GameObject door;
+    [SerializeField] private float openDuration;
+    [SerializeField] private Constants.ESceneType sceneType;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (door)
+        {
+            // 문 열기
+            if (other.CompareTag("Player"))
+            {
+                StartCoroutine(OpenDoor());
+            }            
+        }
+        else
+        {
+            GameManager.Instance.LoadScene(sceneType);
+        }
+    }
+
+    private IEnumerator OpenDoor()
+    {
+        float duration = openDuration;
+        float distance = 3f;
+        Vector3 startPosition = door.transform.position;
+        Vector3 endPosition = startPosition + Vector3.up * distance;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / duration;
+            door.transform.position = Vector3.Lerp(startPosition, endPosition, t);
+            yield return null;
+        }
+        door.transform.position = endPosition;
+        
+        GameManager.Instance.LoadScene(sceneType);
+    }
+}
