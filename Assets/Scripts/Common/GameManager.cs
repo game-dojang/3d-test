58c8e9d8cbb3dfbf2965e5eef9854b459c92f617
goyadb <junil@mocapot.com>
2026-03-17 10:49:28 +0900

    로딩 페이지 구현

--- Assets/Scripts/Common/GameManager.cs
+++ Assets/Scripts/Common/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -14,6 +15,45 @@ public class GameManager : Singleton<GameManager>
         Cursor.lockState = _isCursorLock ? CursorLockMode.None : CursorLockMode.Locked;
         _isCursorLock = !_isCursorLock;
     }
+
+    public void LoadScene(Constants.ESceneType sceneType)
+    {
+        StartCoroutine(LoadSceneAsync(sceneType));
+    }
+
+    private IEnumerator LoadSceneAsync(Constants.ESceneType sceneType)
+    {
+        // 로딩 화면 띄우기
+        var loadingPanelPrefab = Resources.Load<GameObject>("Loading Panel");
+        var loadingPanelObject = Instantiate(loadingPanelPrefab, Canvas.transform);
+        var loadingPanelController = loadingPanelObject.GetComponent<LoadingPanelController>();
+        
+        // 로딩 창 표시
+        bool showDone = false;
+        loadingPanelController.Show(()=> showDone = true);
+        yield return new WaitUntil(() => showDone);
+        
+        // 씬 로드 진행
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneType.ToString());
+        asyncOperation.allowSceneActivation = false;
+
+        while (asyncOperation.progress < 0.9f)
+        {
+            loadingPanelController.SetProgress(asyncOperation.progress);
+            yield return null;
+        }
+        loadingPanelController.SetProgress(1f);
+        asyncOperation.allowSceneActivation = true;
+        
+        // 로딩 창 숨기기
+        bool hideDone = false;
+        loadingPanelController.Hide(()=> hideDone = true);
+        yield return new WaitUntil(() => hideDone);
+        
+        // 로딩 패널 오브젝트 제거
+        Destroy(loadingPanelObject);
+    }
+    
 
     private Canvas GetCanvas()
     {
